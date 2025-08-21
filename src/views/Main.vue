c09c10f2faa97844a80af9ff4f637c8ca5a10568
Sergey E <sibharakter@gmail.com>
2025-08-21 15:59:48 +0700

    init

---
+++ src/views/Main.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="page main-page">
+    <main class="main-page__main section section_horizontal-indents">
+      <h1>Select form</h1>
+    </main>
+  </div>
+</template>
+
+<style scoped></style>
